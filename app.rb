754ad95ef4a37ede24d3ef30c06d03c945448fb4
Andrew Cookson <acookson91@gmail.com>
2016-03-14 16:29:35 +0000

    Added use of shotgun

--- app.rb
+++ app.rb
@@ -1,1 +1,22 @@
 require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "Hello World!"
+end
+
+get '/secret' do
+  "Cheater!"
+end
+
+get '/caspar' do
+  "DOG!"
+end
+
+get '/habits' do
+  "LSD"
+end
+
+get '/funny' do
+  "Andrew"
+end
