cf067eb3d8707a74c678aff2c0896dc12224cb72
Andrew Cookson <acookson91@gmail.com>
2016-03-14 16:51:48 +0000

    Added picture of cat to path

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'shotgun'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello World!"
 end
@@ -14,9 +16,15 @@ get '/caspar' do
 end
 
 get '/habits' do
-  "LSD"
+  "LSD/Men dressed up as cats"
 end
 
 get '/funny' do
   "Andrew"
+end
+
+get '/cat' do
+"<div class='cat'>
+  <img style='border: 10px dotted red;'' src='https://metrouk2.files.wordpress.com/2015/10/cat-dressed-as-a-pirate-just-won-halloween.png'>
+  </div>"
 end
